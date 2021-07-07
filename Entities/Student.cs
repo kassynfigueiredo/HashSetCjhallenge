31e14af4c3162f85d59f1f0af4c14e17a430db7e
kassyn de figueiredo <kassynfigueiredo@outlook.com>
2021-07-07 13:22:48 -0300

    Solving the challenge proposed by Nélio Alves

---
+++ Entities/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashChallenge.Entities
+{
+    class Student
+    {
+        public int Number { get; set; }
+
+        public Student(int number)
+        {
+            Number = number;
+        }
+        public override int GetHashCode()
+        {
+            return Number.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return Number.Equals(other.Number);
+        }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HashChallenge.Entities;
+using System.Collections.Generic;
+
+namespace HashChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students for course A? ");
+            int a = int.Parse(Console.ReadLine());
+
+            HashSet<Student> aStudent = new HashSet<Student>();
+            for (int i = 1; i <= a; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                aStudent.Add(new Student(x));
+            }
+
+            Console.Write("How many students for course B? ");
+            int b = int.Parse(Console.ReadLine());
+
+            HashSet<Student> bStudent = new HashSet<Student>();
+            for (int i = 1; i <= b; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                bStudent.Add(new Student(x));
+            }
+
+            Console.Write("How many students for course C? ");
+            int c = int.Parse(Console.ReadLine());
+
+            HashSet<Student> cStudent = new HashSet<Student>();
+            for (int i = 1; i <= c; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                cStudent.Add(new Student(x));
+            }
+
+            HashSet<Student> d = new HashSet<Student>(a);
+            d.UnionWith(bStudent);
+            d.UnionWith(cStudent);
+
+            Console.WriteLine($"Total student: {d.Count}");
+        }
+    }
+}
